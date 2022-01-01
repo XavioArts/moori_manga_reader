bd604797a2dc1ae2e822df920a01231a9901c561
Fabio Xavier <fabio.g.xavier33@gmail.com>
2021-12-31 17:22:18 -0700

    comics controller &publications controller

--- app/controllers/api/comics_controller.rb
+++ app/controllers/api/comics_controller.rb
@@ -1,6 +1,7 @@
 class Api::ComicsController < ApplicationController
 
     before_action :authenticate_user!
+    before_action :set_comic, only: [:show, :update, :destroy]
 
     def index
         render json: current_user.comics
@@ -8,6 +9,45 @@ class Api::ComicsController < ApplicationController
 
     def favorites
         render json: current_user.favorite_comics
+    end
+
+    def comics_full
+        render json: Comic.all
+    end
+
+    def show
+        render json: @comic
+    end
+
+    def create
+        @comic = Comic.new(comic_params)
+        if @comic.save
+            render json: @comic
+        else
+            render json: {errors: @comic.errors}, status: 422
+        end
+    end
+
+    def update
+        if @comic.update(comic_params)
+            render json: @comic
+        else
+            render json: {errors: @comic.errors}, status: 422
+        end
+    end
+
+    def destroy
+        render json: @comic.destroy
+    end
+
+    private
+
+    def set_comic 
+        @comic = Comic.find(params[:id])
+    end
+
+    def comic_params
+        params.require(:comic).permit(:title, :author, :icon, :cover, :summary)
     end
 
 end
--- app/controllers/api/publications_controller.rb
+++ app/controllers/api/publications_controller.rb
@@ -1,2 +1,41 @@
 class Api::PublicationsController < ApplicationController
+
+    before_action :authenticate_user!
+    before_action :set_publication, only: [:destroy]
+
+    def index
+        render json: current_user.publications
+    end
+
+    def create
+        @publication = current_user.publications.new(publication_params)
+        if @publication.save
+            render json: @publication
+        else
+            render json: {errors: @publication.errors}, status 422
+        end
+    end
+    # def create
+    #     @publication = Publication.new(publication_params)
+    #     if @publication.save
+    #         render json: @publication
+    #     else
+    #         render json: {errors: @publication.errors}, status 422
+    #     end
+    # end
+
+    def destroy
+        render json: @publication.destroy
+    end
+
+    private
+
+    def publication_params
+        params.require(:publication).permit(:title, :user_id, :comic_id)
+    end
+
+    def set_publication
+        @publication = Publication.find(params[:id])
+    end
+
 end
