2d0268349ff65bd84f7dfc140c4e99bc2def662a
Fabio Xavier <fabio.g.xavier33@gmail.com>
2022-03-09 20:14:32 -0700

    Navbar redone

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,17 +1,61 @@
-import React, { useContext } from "react";
-import { Link } from "react-router-dom";
+import React, { useContext, useEffect, useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { AuthContext } from "../providers/AuthProvider";
+import { Menu } from "antd";
+import { AppstoreOutlined, BookOutlined, EllipsisOutlined, GlobalOutlined, InsertRowAboveOutlined, LoginOutlined, LogoutOutlined, ReadOutlined, SettingOutlined, ShopOutlined, SlidersOutlined, UserOutlined } from "@ant-design/icons";
 
 const NavBar = () => {
 
     const { authenticated } = useContext(AuthContext);
+    const [current, setCurrent] = useState("");
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    useEffect(()=>{
+        console.log(location.pathname);
+        setCurrent(location.pathname);
+    }, []);
+
+    const handleClick = (e) => {
+        console.log("click", e);
+        setCurrent(e.key);
+        navigate(e.key);
+    }
 
     return (
         <div style={styles.container} > 
-            <Link to="/" style={styles.link} >Home</Link>
+            {/* <Link to="/" style={styles.link} >Home</Link>
             <Link to="/public" style={styles.link} >Public</Link>
             {authenticated && <Link to="/protected" style={styles.link} >Protected</Link>}
-            {authenticated && <Link to="/cont_test" style={styles.link} >Test All</Link>}
+            {authenticated && <Link to="/cont_test" style={styles.link} >Test All</Link>} */}
+            <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal" >
+                {authenticated && <><Menu.Item key="/" icon={<InsertRowAboveOutlined/>} >
+                    Home
+                </Menu.Item>
+                <Menu.Item key="/publications" icon={<BookOutlined/>} >
+                    Publications
+                </Menu.Item>
+                <Menu.Item key="/library" icon={<ReadOutlined/>} >
+                    My Library
+                </Menu.Item></>}
+                <Menu.Item key="/explore" icon={<GlobalOutlined/>} >
+                    Explore
+                </Menu.Item>
+                <Menu.Item key="/store" icon={<ShopOutlined/>} >
+                    Store
+                </Menu.Item>
+                {!authenticated && <Menu.Item key="/login" icon={<LoginOutlined/>} >
+                    Login
+                </Menu.Item>}
+                {authenticated && <Menu.SubMenu key="SubMenu" title="Settings" icon={<SettingOutlined/>} >
+                    <Menu.Item key="/profile" icon={<EllipsisOutlined/>} >
+                        Profile
+                    </Menu.Item>
+                    <Menu.Item key="logout" icon={<LogoutOutlined/>} >
+                        Logout
+                    </Menu.Item>
+                </Menu.SubMenu>}
+            </Menu>
         </div>
     );
 };
@@ -21,7 +65,7 @@ const styles = {
         margin: "0px",
         padding: "10px",
         textAlign: "center",
-        backgroundColor: "black",
+        // backgroundColor: "black",
     },
     link: {
         textDecoration: "none",
--- client/src/components/RequireAuth.js
+++ client/src/components/RequireAuth.js
@@ -1,18 +1,36 @@
-import React, { useContext } from "react";
-import { Navigate, Outlet } from "react-router-dom";
+import React, { useContext, useEffect, useState } from "react";
+import { Navigate, Outlet, useNavigate } from "react-router-dom";
 import { AuthContext } from "../providers/AuthProvider";
 
 const RequireAuth = () => {
 
     const auth = useContext(AuthContext);
+    const navigate = useNavigate();
+    const [redirect, setRedirect] = useState(false);
+    let timer = null;
+
+    useEffect(()=>{
+        return () => {
+            clearTimeout(timer);
+        }
+    }, [])
+
+    const timeRedirect = () => {
+        timer = setTimeout(()=>setRedirect(true), 4500);
+    }
 
     if (!auth.authenticated) {
-        return <Navigate to="/" />;
+        return (
+            <div>
+                <h3>Loading...</h3>
+                {timeRedirect()}
+                {redirect && <Navigate to="/login" />}
+            </div>
+        )
     }
 
     return (
         <div>
-            <h1>Welcome {auth.email}</h1>
             <Outlet />
         </div>
     );
